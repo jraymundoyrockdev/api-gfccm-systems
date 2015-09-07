51285df2c51a01c91818fafa689a7bb83139833e
jem_nrns <jraymundo@nrnssoftware.com.au>
2015-09-07 08:44:03 +0000

    Finish Services API

--- app/Http/Controllers/Api/ServicesController.php
+++ app/Http/Controllers/Api/ServicesController.php
@@ -37,64 +37,47 @@ class ServicesController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
-     * @param  Request  $request
+     * @param  Request $request
      * @return Response
      */
     public function store(Request $request)
     {
-        //
+        $input = array_filter($request->request->all());
+
+        return (new ItemResponse($this->service->createNewService($input)))->asType('Service');
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return Response
      */
     public function show($id)
     {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return Response
-     */
-    public function edit($id)
-    {
-        //
+        return new ItemResponse($this->service->getById($id));
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  Request  $request
-     * @param  int  $id
+     * @param  Request $request
+     * @param  int $id
      * @return Response
      */
     public function update(Request $request, $id)
     {
-        //
+        $input = array_filter($request->request->all());
+
+        return (new ItemResponse($this->service->updateService($id, $input)))->asType('Service');
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return Response
      */
     public function destroy($id)
