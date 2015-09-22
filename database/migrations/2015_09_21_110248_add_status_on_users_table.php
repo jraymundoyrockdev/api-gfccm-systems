bb148a64f0df492f830f365743939fe543aaf13d
jem_nrns <jraymundo@nrnssoftware.com.au>
2015-09-22 05:12:33 +0000

    change user status to string than integer

--- database/migrations/2015_09_21_110248_add_status_on_users_table.php
+++ database/migrations/2015_09_21_110248_add_status_on_users_table.php
@@ -13,7 +13,7 @@ class AddStatusOnUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->integer('status')->after('username');
+            $table->string('status', 10)->after('username')->default('inactive');
         });
 
     }
