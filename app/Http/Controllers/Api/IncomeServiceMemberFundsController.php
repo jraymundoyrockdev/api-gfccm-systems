407fd981c59551191424e83b6d8a9a5758c23640
Jem Raymundo <jraymundo.yrockdev@gmail.com>
2016-01-28 08:30:36 +0000

    [Technical debt] Validate User Existence on IS
    Resolves: ticket #33

--- app/Http/Controllers/Api/IncomeServiceMemberFundsController.php
+++ app/Http/Controllers/Api/IncomeServiceMemberFundsController.php
@@ -1,13 +1,29 @@
 <?php namespace ApiGfccm\Http\Controllers\Api;
 
-use ApiGfccm\Commands\UpdateIncomeServiceMemberFund;
 use ApiGfccm\Commands\DeleteIncomeServiceMemberFundTotal;
+use ApiGfccm\Commands\UpdateIncomeServiceMemberFund;
 use ApiGfccm\Http\Controllers\Controller;
 use ApiGfccm\Http\Requests;
+use ApiGfccm\Repositories\Interfaces\IncomeServiceMemberFundRepositoryInterface;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class IncomeServiceMemberFundsController extends Controller
 {
+    /**
+     * @var IncomeServiceMemberFundRepositoryInterface
+     */
+    protected $incomeService;
+
+    /**
+     * @param IncomeServiceMemberFundRepositoryInterface $incomeService
+     * @internal param $IncomeServiceMemberFundRepositoryInterface $
+     */
+    public function __construct(IncomeServiceMemberFundRepositoryInterface $incomeService)
+    {
+        $this->incomeService = $incomeService;
+    }
+
     /**
      * Updates Member Funds and Calculate Amount
      *
@@ -21,7 +37,7 @@ class IncomeServiceMemberFundsController extends Controller
         $validate = $this->validateUpdateMemberIncomeService(array_shift($input));
 
         if (!empty($validate)) {
-            return response($validate, 422);
+            return (new Response())->setContent($validate)->setStatusCode(422);
         }
 
         return response()->json(($this->dispatch(
@@ -37,7 +53,7 @@ class IncomeServiceMemberFundsController extends Controller
      * @param $memberId
      * @return \Illuminate\Http\JsonResponse
      */
-    public function deleteMemberFund($incomeServiceId,$memberId)
+    public function deleteMemberFund($incomeServiceId, $memberId)
     {
         return response()->json(($this->dispatch(
             new DeleteIncomeServiceMemberFundTotal($incomeServiceId, $memberId)
@@ -53,13 +69,31 @@ class IncomeServiceMemberFundsController extends Controller
     private function validateUpdateMemberIncomeService($input)
     {
         if (empty($input['member_id'])) {
-            return ['message' => 'Validation Error', 'errors' => ['member_id' => 'Member does not exists']];
+            return ['message' => 'Validation Error', 'errors' => ['member_id' => ['Member does not exists.']]];
         }
 
         if (empty($input['income_service_id'])) {
-            return ['message' => 'Validation Error', 'errors' => ['income_service_id' => 'Income Service does not exists']];
+            return ['message' => 'Validation Error', 'errors' => ['income_service_id' => ['Income Service does not exists.']]];
+        }
+
+        $member = $this->isMemberExits($input['member_id'], $input['income_service_id']);
+
+        if ($member) {
+            return ['message' => 'Validation Error', 'errors' => ['member_id' => ['Member already Exists.']]];
         }
 
         return [];
+    }
+
+    /**
+     * Check for member duplication
+     *
+     * @param $memberId
+     * @param $incomeServiceId
+     * @return mixed
+     */
+    private function isMemberExits($memberId, $incomeServiceId)
+    {
+        return $this->incomeService->getByIdAndMemberId($memberId, $incomeServiceId);
     }
 }
